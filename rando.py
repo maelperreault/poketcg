3e6f6872686bf674f2e1f65f62617f0471707813
sushi <sushi@rainbowism.com>
2021-03-17 00:19:02 -0400

    Add bank03 randomizing

--- rando.py
+++ rando.py
@@ -30,15 +30,23 @@ def noise3d(x, y, z, seed=0):
 def pick(l, n):
     return l[n % len(l)]
 
+def calc_prize(p, r, n):
+    offset = n % (r * 2) - r
+    value = p + offset
+    if value < 1:
+        return 1
+    elif value > 6:
+        return 6
+    return value
 
-sets = [
-    'COLOSSEUM',
-    'EVOLUTION',
-    'MYSTERY',
-    'LABORATORY',
-]
+def calc_range(p, min, max):
+    return p % (max - min + 1) + min
 
-def randomize_cards(conf):
+RAND_CARDS = 10
+RAND_DECKS = 20
+RAND_BOOSTERS = 30
+
+def randomize_cards(conf, data):
     current_card = ''
     with open('templates/cards.asm', 'r') as src, open('src/data/cards.asm', 'w') as target:
         for i, line in enumerate(src):
@@ -49,16 +57,74 @@ def randomize_cards(conf):
                 # @Todo add rarity randomizing?
                 target.write(line)
             elif 'RANDOMIZE_SET' in line:
-                y = conf['cards'][current_card]['group'] if conf['cards_by_evo'] else conf['cards'][current_card]['id']
-                target.write('	db %s | NONE ; sets\n' % pick(sets, noise2d(10, y)))
+                y = data['cards'][current_card]['group'] if conf['cards_by_evo'] else data['cards'][current_card]['id']
+                y *= 10
+                target.write('	db %s | NONE ; sets\n' % pick(data['sets'], noise2d(RAND_CARDS, y, conf['seed'])))
             else:
                 target.write(line)
 
-cards = None
-with open('cards.json') as file:
-    cards = json.load(file)
+def randomize_bank03(conf, data):
+    with open('templates/bank03.asm', 'r') as src, open('src/engine/bank03.asm', 'w') as target:
+        for i, line in enumerate(src):
+            if '; DUEL:' in line:
+                trainer = data['npc_duels'][line.split(':')[1].strip()]
+                y = trainer['id'] * 10
+                prize = trainer['prize']
+                deck_id = trainer['deck_id']
+                music_id = trainer['music_id']
+                if not conf['exclude_prize']:
+                    prize = calc_prize(prize, conf['prize_range'], noise3d(RAND_DECKS, y, 10, conf['seed']))
+                if not conf['exclude_decks']:
+                    deck_id = pick(data['decks'], noise3d(RAND_DECKS, y, 20, conf['seed']))
+                if not conf['exclude_music']:
+                    music_id = pick(data['music'], noise3d(RAND_DECKS, y, 30, conf['seed']))
+                target.write('	start_duel PRIZES_{}, {}, {}\n'.format(prize, deck_id, music_id))
+            elif '; BOOSTERS:' in line:
+                trainer = data['npc_boosters'][line.split(':')[1].strip()]
+                y = trainer['id'] * 10
+                z = 10
+                booster_count = 0
+                boosters = []
+                if conf['exclude_boosters']:
+                    boosters = trainer['packs']
+                else:
+                    booster_count = calc_range(noise3d(RAND_BOOSTERS, y, z, conf['seed']), conf['booster_min'], conf['booster_max'])
+                    for i in range(booster_count):
+                        z += 10
+                        boosters.append(pick(data['packs'], noise3d(RAND_BOOSTERS, y, z, conf['seed'])))
+
+                remainder = booster_count % 3
+                if remainder > 0:
+                    boosters += ['NO_BOOSTER'] * (3 - remainder)
+                booster_count = len(boosters)
+
+                while len(boosters) >= 3:
+                    booster_set = boosters[:3]
+                    boosters = boosters[3:]
+                    target.write('	give_booster_packs {}\n'.format(', '.join(booster_set)))
+            else:
+                target.write(line)
+
+data = None
+with open('data.json') as file:
+    data = json.load(file)
+
+seed = 1
 
 randomize_cards({
-    'cards': cards,
-    'cards_by_evo': False,
-})
+    'seed': seed,
+    'cards_by_evo': True
+}, data)
+
+randomize_bank03({
+    'seed': seed,
+
+    'prize_range': 1,
+    'exclude_prize': False,
+    'exclude_decks': False,
+    'exclude_music': False,
+
+    'booster_min': 2,
+    'booster_max': 7,
+    'exclude_boosters': False,
+}, data)
